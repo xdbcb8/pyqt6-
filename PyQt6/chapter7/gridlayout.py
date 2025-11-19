030ebdae4584f130c20f3dae2f8a382ba62bdfe5
xdbcb8 <xdbcb8@qq.com>
2025-11-19 15:19:01 +0800

    增加部分代码注释

--- PyQt6/chapter7/gridlayout.py
+++ PyQt6/chapter7/gridlayout.py
@@ -8,6 +8,7 @@
 @微信公众号:  学点编程吧
 '''
 # 网格布局举例——模拟小键盘
+# 完整程序位于本书配套资料的PyQt6\chapter7\gridlayout.py中
 
 import sys
 from PyQt6.QtWidgets import QApplication, QWidget, QGridLayout, QSizePolicy, QToolButton
@@ -27,6 +28,7 @@ class MyWidget(QWidget):
         self.initUI()
 
     def initUI(self):
+        self.resize(350, 300)
         self.setWindowTitle("栅格布局：小键盘模拟")
         digitButtons = []
         for i in range(10):  # 构造0-9这10个数字按钮
@@ -41,17 +43,17 @@ class MyWidget(QWidget):
         pointButton = Button(".", self)
 
         # 将按钮放到各自的位置
-        layout = QGridLayout(self)
+        layout = QGridLayout(self) # 创建网络布局
         layout.addWidget(lockButton, 0, 0)
         layout.addWidget(divButton, 0, 1)
         layout.addWidget(timesButton, 0, 2)
         layout.addWidget(minusButton, 0, 3)
-        layout.addWidget(plusButton, 1, 3, 2, 1)
-        layout.addWidget(enterButton, 3, 3, 2, 1)
+        layout.addWidget(plusButton, 1, 3, 2, 1) # 跨行的布局
+        layout.addWidget(enterButton, 3, 3, 2, 1) # 跨行的布局
         layout.addWidget(pointButton, 4, 2)
-        layout.addWidget(digitButtons[0], 4, 0, 1, 2)
+        layout.addWidget(digitButtons[0], 4, 0, 1, 2) # 跨列的布局
 
-        # 将1-9按钮放到各自的位置
+        # 使用循环的方式将数字按钮1～9分别按照特定的坐标放在相应的单元格中
         n = 1
         while(n < 10):
             for i in range(3, 0, -1):
